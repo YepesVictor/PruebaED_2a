a21da88db7bf05dfa0cd5ba4c6f71aecf96d2654
VictorYepes <voyepesl01@larioja.edu.es>
2024-04-09 21:22:29 +0200

    JavaDoc

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"clases"},{"l":"org.example"}];updateSearchResults();
---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"clases","l":"Curso"},{"p":"org.example","l":"Main"},{"p":"clases","l":"Nif"},{"p":"clases","l":"Persona"}];updateSearchResults();
