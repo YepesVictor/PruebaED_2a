20e7f9ec72c5e5cb35c66e42faf80a3cbfb67993
VictorYepes <voyepesl01@larioja.edu.es>
2024-04-09 21:14:51 +0200

    Documentacion <Curso>

--- src/main/java/clases/Curso.java
+++ src/main/java/clases/Curso.java
@@ -8,7 +8,7 @@ import java.util.TreeSet;
 
 /**
  *
- * @author ProfDiurno
+ * @author VictorYepes
  */
 public class Curso {
 
@@ -19,12 +19,21 @@ public class Curso {
         return nombre;
     }
 
-
+    /**
+     * Constructor que recibe un String el cual va a ser el nombre del curso y
+     * crea un TreeSet que almacena objetos Persona los cuales seran los alumnos del curso
+     *
+     * @param nombre String que sera el nombre del curso
+     */
     public Curso(String nombre) {
         this.nombre = nombre;
         listaAlumnos = new TreeSet<>();
     }
 
+    /**
+     * Muestra el Curso y una lista de los alumnos que estan es este
+     * @return String con el nombre del curso y la lista de los alumnos
+     */
     @Override
     public String toString() {
         String s = "--------------------" + nombre + "-----------------\n";
@@ -36,6 +45,10 @@ public class Curso {
         return s;
     }
 
+    /**
+     * Añade un objeto Persona al TreeSet de lista de alumnos
+     * @param p Objeto Persona que se añadira en el TreeSet
+     */
     public void aniadirAlumno(Persona p) {
         listaAlumnos.add(p);
     }
