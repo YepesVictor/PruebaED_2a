0857a6c3b1280b8e44752e7b8b1cbb55478e42e0
VictorYepes <voyepesl01@larioja.edu.es>
2024-04-09 20:48:05 +0200

    Documentacion <NIF>

--- src/main/java/clases/Nif.java
+++ src/main/java/clases/Nif.java
@@ -6,7 +6,7 @@ package clases;
 
 /**
  *
- * @author ProfDiurno
+ * @author VictorYepes
  */
 public class Nif {
 
@@ -19,20 +19,36 @@ public class Nif {
                 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L',
                 'C', 'K', 'E'};
 
+    /**
+     * Metodo que evuelve la letra correspondiente segun el resto de dividir un nemero entre 23
+     * @param numero DNI del cual se quiere saber cual es la letra que le corresponde
+     * @return Un char con la letra que le corresponde al numero DNI
+     */
     private static char calcularLetra(int numero) {
         return LETRAS[numero % 23];
     }
 
+    /**
+     * Constructor que no recibe ningun parametro y que inicializa el numero en cero (0) y la letra  con un espacio en blanco (' ')
+     */
     protected Nif() {
         this.numero = 0;
         this.letra = ' ';
     }
 
+    /**
+     * Constructor que recibe un numero de DNI y le calcula la letra correspondiente
+     * @param numero Numero de DNI
+     */
     protected Nif(int numero) {
         this.numero = numero;
         this.letra = calcularLetra(numero);
     }
 
+    /**
+     *
+     * @return Retorna un String con el numero de DNI seguido de la letra que le corresponde Ej: 00000000-X
+     */
     @Override
     public String toString() {
         return numero + "-" + letra;
@@ -43,6 +59,12 @@ public class Nif {
         this.letra = calcularLetra(numero);
     }
 
+    /**
+     *
+     * @param obj Objeto que se quiere saber si es igual al objeto this
+     * @return Retorna true si son el mismo objeto, y si el numero de DNI y la letra coinciden.
+     * Retorna false en cualquier otro caso
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
